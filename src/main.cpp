0540f561000f8aeb338de8bcda719c1b58d9770c
Samu Juvonen <samu.juvonen@gmail.com>
2016-11-02 22:57:01 +0200

    App details

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include "mainwindow.hpp"
 
 int main(int argc, char ** args) {
+    QApplication::setOrganizationName("Juvonet");
+    QApplication::setOrganizationDomain("juvonet.fi");
+    QApplication::setApplicationName("Coquillo");
+    QApplication::setApplicationVersion("1.9");
+    
     QApplication app(argc, args);
     Coquillo::MainWindow window;
     window.show();
