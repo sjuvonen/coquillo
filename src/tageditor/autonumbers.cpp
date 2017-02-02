bab5ea0eb7928aad824232bffb13ae4653eac596
Samu Juvonen <samu.juvonen@gmail.com>
2017-02-02 22:04:35 +0200

    Implement strategies to generate track numbers

--- src/tageditor/autonumbers.cpp
+++ src/tageditor/autonumbers.cpp
@@ -6,6 +6,13 @@
 
 namespace Coquillo {
     namespace TagEditor {
+        using Coquillo::Tags::TagDataRoles;
+
+        AutoNumbers::AutoNumbers(QAbstractItemModel * model)
+        : _model(model) {
+            generateCache();
+        }
+
         void AutoNumbers::autoNumberItems(QAbstractItemModel * model, const QModelIndexList & rows) {
             AutoNumbers instance;
             instance.setModel(model);
@@ -14,6 +21,7 @@ namespace Coquillo {
 
         void AutoNumbers::setModel(QAbstractItemModel * model) {
             _model = model;
+            generateCache();
         }
 
         QAbstractItemModel * AutoNumbers::model() const {
@@ -29,11 +37,14 @@ namespace Coquillo {
         }
 
         void AutoNumbers::generateNumbering(const QString & directory, const QModelIndexList & items) {
-            for (int i = 0; i < items.count(); i++) {
-                const QModelIndex trnum = items[i].sibling(items[i].row(), 7);
-                const QModelIndex trcnt = items[i].sibling(items[i].row(), 8);
-                _model->setData(trnum, i+1);
-                _model->setData(trcnt, items.count());
+            QMap<int, int> numbers;
+            numbers.unite(NumberStrategy::ItemOrderStrategy().suggestions(items));
+            numbers.unite(NumberStrategy::FileNumberStrategy().suggestions(items));
+            numbers.unite(NumberStrategy::PreserveOriginalNumbers().suggestions(items));
+
+            foreach (int i, numbers.keys()) {
+                const QModelIndex idx = items[i].sibling(items[i].row(), Tags::NumberField);
+                model()->setData(idx, numbers[i]);
             }
         }
 
@@ -44,6 +55,60 @@ namespace Coquillo {
                 paths[path].append(idx);
             }
             return paths;
+        }
+
+        void AutoNumbers::generateCache() {
+            _cache.clear();
+
+            for (int i = 0; i < model()->rowCount(); i++) {
+                const QPersistentModelIndex idx = model()->index(i, 0);
+                const QString path = QFileInfo(idx.data(Tags::FilePathRole).toString()).absolutePath();
+                _cache.insert(path, idx);
+            }
+        }
+
+        namespace NumberStrategy {
+            QMap<int, int> ItemOrderStrategy::suggestions(const QModelIndexList & items) {
+                QMap<int, int> numbers;
+
+                for (int i = 0; i < items.size(); i++) {
+                    numbers.insert(i, i + 1);
+                }
+
+                return numbers;
+            }
+
+            QMap<int, int> FileNumberStrategy::suggestions(const QModelIndexList & items) {
+                QMap<int, int> numbers;
+                QRegExp matcher("^(\\d+)[(\\.)(\\s-\\s)]");
+
+                for (int i = 0; i < items.size(); i++) {
+                    const QString filename = items[i].data(TagDataRoles::FileNameRole).toString();
+                    if (matcher.indexIn(filename.trimmed()) != -1) {
+                        numbers.insert(i, matcher.cap(1).toInt());
+                    }
+                }
+
+                return numbers;
+            }
+
+            QMap<int, int> PreserveOriginalNumbers::suggestions(const QModelIndexList & items) {
+                QMap<int, int> numbers;
+
+                for (int i = 0; i < items.size(); i++) {
+                    const QVariantHash values = items[i].data(TagDataRoles::ValuesMapRole).toHash();
+
+                    if (values.contains("number")) {
+                        int number = values["number"].toInt();
+
+                        if (number > 0) {
+                            numbers.insert(i, number);
+                        }
+                    }
+                }
+
+                return numbers;
+            }
         }
     }
 }
--- src/tageditor/autonumbers.hpp
+++ src/tageditor/autonumbers.hpp
@@ -3,6 +3,7 @@
 
 #include <QHash>
 #include <QModelIndexList>
+#include <QMultiHash>
 #include <QPointer>
 
 class QAbstractItemModel;
@@ -11,7 +12,10 @@ namespace Coquillo {
     namespace TagEditor {
         class AutoNumbers {
             public:
+                enum Strategy {ItemOrder, FileNumber, Preserve};
+
                 AutoNumbers() { }
+                AutoNumbers(QAbstractItemModel * model);
                 void setModel(QAbstractItemModel * model);
                 QAbstractItemModel * model() const;
                 void autoNumberItems(const QModelIndexList & rows);
@@ -19,10 +23,46 @@ namespace Coquillo {
                 static void autoNumberItems(QAbstractItemModel * model, const QModelIndexList & rows);
 
             private:
+                void generateCache();
                 void generateNumbering(const QString & directory, const QModelIndexList & items);
                 QPointer<QAbstractItemModel> _model;
                 QHash<QString, QModelIndexList> groupByPath(const QModelIndexList & items) const;
+
+                QMultiHash<QString, QPersistentModelIndex> _cache;
         };
+
+        namespace NumberStrategy {
+            class AbstractStrategy {
+                public:
+                    virtual ~AbstractStrategy() { }
+                    virtual QMap<int, int> suggestions(const QModelIndexList & items) = 0;
+            };
+
+            /**
+             * Generates track numbers based on item order in source list.
+             */
+            class ItemOrderStrategy : public AbstractStrategy {
+                public:
+                    QMap<int, int> suggestions(const QModelIndexList & items);
+            };
+
+            /**
+             * Parse track numbers from filenames.
+             */
+            class FileNumberStrategy : public AbstractStrategy {
+                public:
+                    QMap<int, int> suggestions(const QModelIndexList & items);
+            };
+
+            /**
+             * Returns track numbers for items that already have it defined.
+             */
+            class PreserveOriginalNumbers : public AbstractStrategy {
+                public:
+                    QMap<int, int> suggestions(const QModelIndexList & items);
+            };
+
+        }
     }
 }
 
