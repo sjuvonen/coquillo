d9101ac2cd3524cc7e84cc4b8a0bba03f6045216
Samu Juvonen <samu.juvonen@gmail.com>
2018-02-04 14:17:29 +0200

    Allow numbers in safe paths

--- src/utils/filepaths.cpp
+++ src/utils/filepaths.cpp
@@ -7,13 +7,13 @@ namespace Coquillo {
     namespace Utils {
         namespace FilePaths {
             QString safeFilePath(const QString & path) {
-                const QRegExp filter("[^\\/a-z \\.\\-_\\$&\\(\\)\\[\\]]", Qt::CaseInsensitive);
+                const QRegExp filter("[^\\/a-z0-9 \\.\\-_\\$&\\(\\)\\[\\]]", Qt::CaseInsensitive);
                 return QDir::toNativeSeparators(QDir::fromNativeSeparators(path).replace(filter, ""));
             }
 
             QString safeFileName(const QString & name) {
                 // NOTE: Same filter as in safeFilePath except for forward slash.
-                const QRegExp filter("[^a-z \\.\\-_\\$&\\(\\)\\[\\]]", Qt::CaseInsensitive);
+                const QRegExp filter("[^a-z0-9 \\.\\-_\\$&\\(\\)\\[\\]]", Qt::CaseInsensitive);
                 return QDir::toNativeSeparators(QDir::fromNativeSeparators(name).replace(filter, ""));
             }
 
